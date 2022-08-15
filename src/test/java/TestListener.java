6e206fcd6990a8b4be4cdbe67aee84f387c02b82
shuvashishny <shuvashishny@gmail.com>
2022-08-15 17:11:14 -0400

    Added screenshots

--- src/test/java/TestListener.java
+++ src/test/java/TestListener.java
@@ -1,4 +1,5 @@
 import browser.Browser;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -10,18 +11,21 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class TestListener  extends TestListenerAdapter {
-    private static final String SRC_FOLDER = "src\\screenshots\\";
-    WebDriver driver = Browser.getDriver();
+    private static final String SRC_FOLDER = "src/screenshots/";
+    WebDriver driver;
 
     public void onTestFailure(ITestResult tr){
-        System.out.println("Test failed "+tr.getTestName());
+        System.out.println("Test failed "+tr.getName());
+        driver = Browser.getDriver();
         File screenShot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-
+        File destFile = new File(SRC_FOLDER+tr.getName()+".png");
         try {
-            Files.copy(screenShot.toPath(), new File(SRC_FOLDER, tr.getTestName()+".png").toPath());
+            FileUtils.copyFile(screenShot, destFile);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("Did not get dest ********");
+            e.printStackTrace();
         }
 
     }
+
 }
